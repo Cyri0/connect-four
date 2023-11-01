bd695fe8745e194e5e10c3823c111938516b2513
Cyri0 <nemestamas94@gmail.com>
2023-11-01 15:27:58 +0100

    diagonal test is done

--- src/calculations/winTest.tsx
+++ src/calculations/winTest.tsx
@@ -1,9 +1,9 @@
 const rowTest = (line: number[]): boolean => {
     let counter: number = 0
-    let sample: number = 0
+    let sample: number = -1
 
     line.forEach(e => {
-        if (sample == 0) {
+        if (sample == -1 && e > 0) {
             sample = e
             counter++
         }
@@ -11,14 +11,21 @@ const rowTest = (line: number[]): boolean => {
             counter++
         }
         else {
-            if(e != 0){
+            if(e > 0){
                 sample = e
                 counter = 1
             }
         }
     })
+
+    // console.log(`${line} - (${counter})`);
+
     if (counter == 4)
-        return true
+        {
+            console.log(`Player${sample} win`);
+            
+            return true
+        }
     return false
 }
 
@@ -41,17 +48,51 @@ const horizontalTest = (m: number[][]) => {
                 line.push(m[row][column])
             }
         }
-        // console.log(line);
-
         if (rowTest(line))
-            console.log(`YOU WIN! - (horizontal)`)
+            {console.log(`YOU WIN! - (horizontal)`)
+            console.log(`${column} - ${line}`)}
     }
-    console.log()
 }
 
+const diagonalTest = (m:number[][]) => {
+   
+    let rowNum = 7
+    let colNum = 6
 
+    for (let row = 0; row <= rowNum - 4; row++) {
+        for (let col = 0; col <= colNum - 4; col++) {
+            // console.log('DIagonal test');
+            
+        //   console.log(`${m[row][col]},${m[row + 1][col + 1]},${m[row + 2][col + 2]},${m[row + 3][col + 3]}`);
+          if (
+            m[row][col] === m[row + 1][col + 1] &&
+            m[row][col] === m[row + 2][col + 2] &&
+            m[row][col] === m[row + 3][col + 3] &&
+            m[row][col] !== undefined
+          ) {
+            return true;
+          }
+        }
+      }
+
+      for (let row = rowNum - 1; row >= 3; row--) {
+        for (let col = 0; col <= colNum - 4; col++) {
+          if (
+            m[row][col] === m[row - 1][col + 1] &&
+            m[row][col] === m[row - 2][col + 2] &&
+            m[row][col] === m[row - 3][col + 3] &&
+            m[row][col] !== undefined
+          ) {
+            return true;
+          }
+        }
+      }
+      return false
+}
 
 export const winTest = (m: number[][]) => {
     verticalTest(m)
     horizontalTest(m)
+    console.log(diagonalTest(m));
+    
 }
